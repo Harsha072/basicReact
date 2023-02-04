6cd1d993c892d47ddb40f4ec5ebbfa90146eab6a
20100308@mail.wit.ie <20100308@mail.wit.ie>
2023-02-04 19:23:11 +0000

    excercise 2 completed

--- src/components/exercises/02_embeddedVariables.js
+++ src/components/exercises/02_embeddedVariables.js
@@ -1,10 +1,51 @@
 import React from "react";
 import "../../../node_modules/bootstrap/dist/css/bootstrap.css";
 
-
 const Demo = () => {
+  const module = [{
+    name: "Devops",
+    lectures: 4,
+    practicles: 3
+  },
+  {
+    name: "Enterprise",
+    lectures: 6,
+    practicles: 2
+  }
+    , {
+      name: "Agile",
+    lectures: 3,
+    practicles: 2
+  },
+            {
+    name: "Mobile",
+    lectures: 4,
+    practicles: 3
+  }]
   return (
-      <h1>TODO</h1>
+    <table className="table table-bordered">
+      <thead>
+        <tr><th><h1>Msc Enterprise Software Systems- Modules Table</h1></th></tr>
+        <tr>
+
+          <th>Name</th>
+          <th>No lectures</th>
+          <th>No practicals</th>
+        </tr>
+      </thead>
+      <tbody >
+        <tr>
+          <td>{module[1].name}</td>
+          <td>{module[1].lectures}</td>
+          <td>{module[1].practicles}</td>
+        </tr>
+        <tr>
+          <td>{module[1].name}</td>
+          <td>{module[1].lectures}</td>
+          <td>{module[1].practicles}</td>
+        </tr>
+      </tbody >
+    </table>
   );
 };
 
